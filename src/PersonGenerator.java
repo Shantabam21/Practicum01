0fb5af54f999fb1b91a05197497a2618453cbf6b
Shantabam21 <kabylbekov07@gmail.com>
2025-08-26 15:05:24 -0700

    I took the individual string object from the arraylist and wrote to a file. Each line would indicate a seperate string object containing several information ie. ID, Name, Last Name, etc..

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardOpenOption.CREATE;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -25,11 +31,42 @@ public class PersonGenerator {
             lastName = SafeInput.getNonZeroLenString(in, "Enter last name: ");
             title = SafeInput.getNonZeroLenString(in, "Enter title: ");
             yearOfBirth = SafeInput.getRangedInt(in,"Enter birth year: ", 1000, 9999);
+
+            String str = ID + ", " + firstName + ", "  + lastName + ", " + title + ", " + yearOfBirth;
+            records.add(str);
             done = SafeInput.getYNConfirm(in, "Enter Yes or No: ");
         }
 
         while (!done);
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt");
 
+            try
+            {
+                // Typical java pattern of inherited classes
+                // we wrap a BufferedWriter around a lower level BufferedOutputStream
+                OutputStream out =
+                        new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+                BufferedWriter writer =
+                        new BufferedWriter(new OutputStreamWriter(out));
+
+                // Finally can write the file LOL!
+
+                for(String record : records)
+                {
+                    writer.write(record, 0, record.length());  // stupid syntax for write rec
+                    // 0 is where to start (1st char) the write
+                    // rec. length() is how many chars to write (all)
+                    writer.newLine();  // adds the new line
+
+                }
+                writer.close(); // must close the file to seal it and flush buffer
+                System.out.println("Data file written!");
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
 
 
     }
